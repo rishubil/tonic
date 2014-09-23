68ee577192f0f9e3689354153e4450e79e3e8dff
Nesswit <rishubil@gmail.com>
2014-09-23 15:05:33 +0000

    added license display

--- app/controllers.py
+++ app/controllers.py
@@ -5,28 +5,78 @@ import random
 import json
 from urllib2 import Request, urlopen
 
-def getPhoto(search_text):
+def getLicenses():
     try:
-        response = None
-        if search_text == u'sky is clear':
-            response = flickr.photos_search(tags='sunny,cloudless',\
-                sort='relevance')
-        else:
-            response = flickr.photos_search(text=search_text, sort='relevance')
+        licenses = {}
+        response = flickr.photos_licenses_getInfo()
         if response.attrib['stat'] == 'ok':
-            photos = response.find('photos')
-            index = random.randint(0, len(photos)-1)
-            photo = photos[index]
-            imageUrl = 'https://farm' + photo.attrib['farm'] + \
-                '.staticflickr.com/' + photo.attrib['server'] + '/' + \
-                photo.attrib['id'] + '_' + photo.attrib['secret'] + '.jpg'
-            return imageUrl
+            result = response.find('licenses')
+            for i in result:
+                index = i.attrib['id']
+                licenses[str(index)] = {'name': i.attrib['name'], 'url': i.attrib['url']}
+            return licenses
         else:
-            print 'Not ok'
-            return "http://"
+            print 'flickr.photos_licenses_getInfo failed'
+            return None
     except Exception, e:
         print "Unexpected error: " + str(e)
-        return "http://"
+        return None
+        
+licenses = getLicenses()
+
+def getPhoto(search_text):
+    def getPhotos(search_text):
+        try:
+            response = None
+            if search_text == u'sky is clear':
+                response = flickr.photos_search(tags='sunny,cloudless',\
+                    sort='relevance', safe_search='1', license='0,1,2,4,5,7,8')
+            else:
+                response = flickr.photos_search(text=search_text, \
+                    sort='relevance', safe_search='1', license='0,1,2,4,5,7,8')
+            if response.attrib['stat'] == 'ok':
+                return response.find('photos')
+            else:
+                print 'flickr.photos_search failed'
+                return None
+        except Exception, e:
+            print "Unexpected error: " + str(e)
+            return None
+            
+    def getPhotoUrl(photo):
+        try:
+            imageUrl = 'https://farm' + photo.attrib['farm'] + \
+                    '.staticflickr.com/' + photo.attrib['server'] + '/' + \
+                    photo.attrib['id'] + '_' + photo.attrib['secret'] + '.jpg'
+            return imageUrl
+        except Exception, e:
+            print "Unexpected error: " + str(e)
+            return "http://"
+    
+    def getPhotoInfo(photoId):
+        try:
+            response = None
+            response = flickr.photos_getInfo(photo_id=photoId)
+            if response.attrib['stat'] == 'ok':
+                return response.find('photo')
+            else:
+                print 'flickr.photos_getInfo failed'
+                return None
+        except Exception, e:
+            print "Unexpected error: " + str(e)
+            return None
+            
+    photos = getPhotos(search_text)
+    index = random.randint(0, len(photos)-1)
+    photo = getPhotoInfo(photos[index].attrib['id'])
+    imageUrl = getPhotoUrl(photo)
+    license = licenses[str(photo.attrib['license'])]
+    # print ET.tostring(photo, encoding='utf8', method='xml')
+    owner = photo.find('owner')
+    author = owner.attrib['username']
+    authorUrl = 'https://www.flickr.com/people/' + owner.attrib['nsid']
+    
+    return {'imageUrl': imageUrl, 'license': license, 'author': author, 'authorUrl': authorUrl}
         
 def getWeather(city):
     weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + \
@@ -34,8 +84,22 @@ def getWeather(city):
     request = Request(weatherUrl)
     try:
     	response = urlopen(request)
-    	weatherJson = response.read()
-    	return json.loads(weatherJson)
+    	weather = json.loads(response.read())
+    	if weather and u'cod' in weather:
+            cod = weather[u'cod']
+            if cod == 200:
+                description = weather[u'weather'][0][u'description']
+                temp = weather[u'main'][u'temp']
+                humidity = weather[u'main'][u'humidity']
+                return {'description': description, 'temp': temp, \
+                'humidity': humidity}
+                
+            else:
+                print "Weather is none: cod is " + str(cod)
+                return None
+        else:
+            print "Weather is none: Unexpected error"
+            return None
     except Exception, e:
         print "Unexpected error: " + str(e)
         return None
@@ -43,13 +107,9 @@ def getWeather(city):
 @app.route('/', methods=['GET'])
 def index():
     weather = getWeather(request.args.get('city', 'seoul'))
-    # print str(weather)
-    if weather:
-        description = weather[u'weather'][0][u'description']
-        temp = weather[u'main'][u'temp']
-        humidity = weather[u'main'][u'humidity']
-        imageUrl = getPhoto(description)
-        return render_template('index.html', imageUrl=imageUrl, \
-            description=description, temp=temp, humidity=humidity)
-    else:
-        return "no weather"
+    if weather == None:
+        return "Weather is none"
+    photo = getPhoto(weather['description'])
+    if weather == None:
+        return "Photo is none"
+    return render_template('index.html', weather=weather, photo=photo)
