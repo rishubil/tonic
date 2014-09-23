7cd7b5310b5ec824ba828a3a43291c78b49f904d
Nesswit <rishubil@gmail.com>
2014-09-23 02:34:41 +0000

    add whether description

--- app/controllers.py
+++ app/controllers.py
@@ -2,24 +2,52 @@ from flask import render_template
 from app import app, flickr
 import xml.etree.ElementTree as ET
 import random
+import json
+from urllib2 import Request, urlopen
 
-@app.route('/')
-def index():
+def getPhoto(search_text):
     try:
-        response = flickr.photos_search(text='seoul rainy')
+        response = None
+        if search_text == u'sky is clear':
+            response = flickr.photos_search(tags='sunny,cloudless',\
+                sort='relevance')
+        else:
+            response = flickr.photos_search(text=search_text, sort='relevance')
         if response.attrib['stat'] == 'ok':
             photos = response.find('photos')
             index = random.randint(0, len(photos)-1)
             photo = photos[index]
             imageUrl = 'https://farm' + photo.attrib['farm'] + \
                 '.staticflickr.com/' + photo.attrib['server'] + '/' + \
-                photo.attrib['id'] + '_' + photo.attrib['secret'] + '_b.jpg'
-            return render_template('index.html', imageUrl=imageUrl)
-            # return imageUrl
+                photo.attrib['id'] + '_' + photo.attrib['secret'] + '.jpg'
+            return imageUrl
         else:
-            return 'Not ok'
+            print 'Not ok'
+            return "http://"
     except Exception, e:
-        return "Unexpected error: " + str(e)
-    # https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_[mstzb].jpg
-    # return flickr.test_echo(boo='baah')
-    # return "Testing for flask"
+        print "Unexpected error: " + str(e)
+        return "http://"
+        
+def getWeather():
+    weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Seoul' + \
+        '&units=metric'
+    request = Request(weatherUrl)
+    try:
+    	response = urlopen(request)
+    	weatherJson = response.read()
+    	return json.loads(weatherJson)
+    except Exception, e:
+        print "Unexpected error: " + str(e)
+        return None
+
+@app.route('/')
+def index():
+    weather = getWeather()
+    # print str(weather)
+    if weather:
+        description = weather[u'weather'][0][u'description']
+        imageUrl = getPhoto(description)
+        return render_template('index.html', imageUrl=imageUrl, \
+            description=description)
+    else:
+        return "no weather"
---
+++ app/static/js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#bg').foggy({
+        blurRadius: 10,
+        opacity: 0.8
+    });
+});
